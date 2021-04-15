ac24bfdba8e523687a38308b2cbd9e3bffd382c1
Tobias Ekman <nilstobiasekman@gmail.com>
2021-04-15 16:28:50 +0200

    menu

--- src/main/java/com/tobiasekman/Menu.java
+++ src/main/java/com/tobiasekman/Menu.java
@@ -8,36 +8,44 @@ public class Menu {
     static Scanner sc = new Scanner(System.in);
     static ArtistManager am = new ArtistManager();
 
+
     public static void menu() throws SQLException {
+        boolean loop = true;
         System.out.println("-Artist management-");
-        System.out.println("[1] Show all artists");
-        System.out.println("[2] Create artist");
-        System.out.println("[3] Update artist");
-        System.out.println("[4] Delete artist");
-        System.out.println("[5] Show artist");
+
+        while (loop) {
+            System.out.println("\n[1] Show all artists");
+            System.out.println("[2] Create artist");
+            System.out.println("[3] Update artist");
+            System.out.println("[4] Delete artist");
+            System.out.println("[5] Show artist");
+            System.out.println("[0] Exit");
 
 
-        int choice = sc.nextInt();
+            int choice = sc.nextInt();
 
-        switch (choice) {
-            case 1:
-                am.showAllArtists();
-                break;
-            case 2:
-                am.createArtist();
-                break;
-            case 3:
-                am.updateArtist();
-                break;
-            case 4:
-                am.deleteArtistById();
-                break;
-            case 5:
-                am.showArtist();
-                break;
-            default:
-                break;
+            switch (choice) {
+                case 1:
+                    am.showAllArtists();
+                    break;
+                case 2:
+                    am.createArtist();
+                    break;
+                case 3:
+                    am.updateArtist();
+                    break;
+                case 4:
+                    am.deleteArtistById();
+                    break;
+                case 5:
+                    am.showArtist();
+                    break;
+                default:
+                    loop = false;
+                    break;
+            }
         }
+
     }
 
 }
